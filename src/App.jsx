3fc7198ba17761024daaaaa687322d4b335f18aa
Bouajila Forkan <bouajilaforkan@gmail.com>
2026-04-20 20:43:27 +0200

    lab3

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,52 @@
-const courseTitle = "React Fundamentals";
+// Each story object contains: title → string, url → string, author → string, objectID → unique identifier (React key), points → number, num_comments → number
+// React key: objectID (unique & stable), APIs return structured JSON data
+
+const stories = [
+  {
+    objectID: "1",
+    title: "fresh book",
+    url: "https://freshmanbook.dev",
+    author: "TBS",
+    points: 120,
+    num_comments: 45,
+  },
+  {
+    objectID: "2",
+    title: "soph book",
+    url: "https://sophomore.org",
+    author: "TBS",
+    points: 150,
+    num_comments: 30,
+  },
+  {
+  objectID: "3",
+  title: "Junior book",
+  url: "https://juniorbook.com",
+  author: "TBS",
+  points: 200,
+  num_comments: 80,
+}
+];
 
 function App() {
-  const studentName = "Forkan";
-
-  const student = {
-    name: "Forkan",
-    age: 20,
-    track: "Web Development"
-  };
-
-  function sayHello() {
-    return "Hello, welcome to React!";
-  }
+  console.log(stories[0]); 
 
   return (
     <div>
-      <h1>My First React Component</h1>
-
-      <p>{studentName}</p>
-
-      <p>{courseTitle}</p>
-
-      <p>
-        Welcome to {courseTitle}, {studentName}!
-      </p>
-
-      <label htmlFor="nameInput">Enter your name:</label>
-      <input type="text" id="nameInput" />
-
-      <p>Name: {student.name}</p>
-      <p>Age: {student.age}</p>
-      <p>Track: {student.track}</p>
-
-      <p>{sayHello()}</p>
+      {stories.map((story) => (
+        <div key={story.objectID}>
+          <h3>
+            <a href={story.url} target="_blank" rel="noreferrer">
+              {story.title}
+            </a>
+          </h3>
+          <p>Author: {story.author}</p>
+          <p>Points: {story.points}</p>
+          <p>Comments: {story.num_comments}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
-
